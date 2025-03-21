aaa13db9e9dc4db86598c8ba4ff4be09590b73fd
ChamodiDilshani <chamodhidilshani2002@gmail.com>
2025-03-21 13:43:07 +0530

    usersCRUD

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,19 +1,52 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const TouristSchema = new Schema({
     name: {
         type: String,
         required: [true, 'Name is required']
     },
     email: {
         type: String,
-        required: [true, 'Email is required']
+        required: [true, 'Email is required'],
+        unique: true
     },
     password: {
         type: String,
-        required: [true, 'Password is required']
+        required: [true, 'Password is required'],
+        select: false
+    },
+    destination: {
+        type: String,
+        enum: ['Beach', 'Mountains', 'City', 'Countryside'],
+        required: true
+    },
+    traveling_with: {
+        type: String,
+        enum: ['Solo', 'Family', 'Partner', 'Friends'],
+        required: true
+    },
+    accommodations: {
+        type: Boolean,
+        required: true
+    },
+    tour_guide: {
+        type: Boolean,
+        required: true
+    }
+}, { timestamps: true });
+
+// Hash password before saving
+TouristSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
     }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('Tourist', TouristSchema);
--- routes/GuideRoutes.js
+++ routes/GuideRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const GuideController = require('../controllers/GuideControllers');
 
 router.get('/', GuideController.getAllGuides);
+router.post('/create',GuideController.createGuide);
+router.put('/update/:id',GuideController.updateGuide);
+router.delete('/delete/:id',GuideController.deleteGuide);
 
 module.exports = router;
