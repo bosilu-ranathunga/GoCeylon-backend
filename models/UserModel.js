afd8112a8b696a0fe90bcace0c2a0801a181743d
Bosilu Ranathunga <rabjinajith@gmail.com>
2025-03-28 22:28:41 +0530

    remove bycript

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const TouristSchema = new Schema({
@@ -39,14 +38,7 @@ const TouristSchema = new Schema({
 
 // Hash password before saving
 TouristSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+
 });
 
 module.exports = mongoose.model('Tourist', TouristSchema);
